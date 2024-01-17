e3c601eed7bc6a84c6a238c5cff447304940b0fb
Grant Meyer <grantmeyer@GrantMeyersMBP.hsd1.ca.comcast.net>
2024-01-17 09:58:16 -0800

    fix

--- scripts/GraphQLmod.mjs
+++ scripts/GraphQLmod.mjs
@@ -397,15 +397,28 @@ async function assignToIssue(repoID, issueID, assignees) {
   try {
     const response = await octokit.graphql({
       query: `
-      mutation {
-        updateIssue(input: {
-          repositoryId:"${repoID}",
-          issueID: "${issueID}",
-          assigneeIds: '${assignees}',
-        })
+    mutation UpdateIssue($repoID: ID!, $issueID: ID!, $assignees: [ID!]!) {
+      updateIssue(input: {
+        id: $issueID,
+        assigneeIds: $assignees
+      }) {
+        issue {
+          number
+          title
+          assignees(first: 10) {
+            nodes {
+              login
+            }
+          }
         }
       }
+    }
   `,
+      variables: {
+        repoID,
+        issueID,
+        assignees,
+      },
     });
   } catch (error) {
     console.error('Error adding assignee to issue', error.message);
