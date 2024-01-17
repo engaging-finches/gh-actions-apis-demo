1b14ea403286955cc9898b983874beca790c3e9e
Grant Meyer <grantmeyer@GrantMeyersMBP.hsd1.ca.comcast.net>
2024-01-17 10:55:55 -0800

    fix

--- scripts/edit-issue.js
+++ scripts/edit-issue.js
@@ -37,17 +37,11 @@ async function main() {
       console.log(`id: ${extracted[i]}`);
       const user_id = await getUserId(extracted[i]);
       console.log(`userid: ${user_id}`);
-      assignToIssue(
-        repo_id,
-        issue_id,
-        title_without_assignees,
-        'body',
-        user_id
-      );
+      assignToIssue(issue_id, user_id);
     }
-    // if (extracted.length > 0) {
-    //   changeIssueTitle(repo_id, issue_id, title_without_assignees);
-    // }
+    if (extracted.length > 0) {
+      changeIssueTitle(repo_id, issue_id, title_without_assignees);
+    }
   } catch (error) {
     console.error('Error:', error.message);
   }
